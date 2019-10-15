66bd9b4fe9e35994041d5b897a3c0d0120f02b1e
Daiki <daiking_tensai@yahoo.co.jp>
2019-10-15 15:18:45 +0900

    add update and display demo

--- examples/sdcard_firmware_update/sdcard_firmware_update.ino
+++ examples/sdcard_firmware_update/sdcard_firmware_update.ino
@@ -5,14 +5,14 @@
   https://dl.espressif.com/dl/package_esp32_index.json
 
  Arduino Settings(use Arduino 1.8.5):    
-  ボード ESP32 Dev Module
+  Board ESP32 Dev Module
   Flash Mode  QIO
   Flash Frequency 40MHz
   CPU Frequency 240MHz
   Flash Size  4M (32Mb)
-  Partition Scheme  Huge App (3MB No OTA/1MB SPIFFS)
+  Partition Scheme  Default 4MB with spiffs(1.2MB APP / 1.5MB SPIFFS)
   Upload Speed  115200
-  Core Debug Level なし
+  Core Debug Level non
 
  The circuit:
  * SD card attached to SPI bus as follows:
@@ -23,18 +23,12 @@
 
  */
 
-#include "Update.h"
- 
-#include <WiFiMulti.h>
+#include <Update.h>
 #include <TFT_eSPI.h>
-
-#include "esp_partition.h"
-#include "esp_ota_ops.h"
 
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
-
 SPIClass spiSD(HSPI);
 #define SD_CLK 14
 #define SD_MISO 2
@@ -42,329 +36,132 @@ SPIClass spiSD(HSPI);
 #define SD_SS 13
 #define SDSPEED 27000000
 
-#define TEXT_FILE_NAME "/hello.txt"
-#define CONFIG_FILE_NAME "/config.txt"
-
-#define SW_VERSION "VERSION001"
-
-typedef struct {
-    char *strMD5;
-}DlInfo;
-
-typedef enum {
-    DownloadingAndSaving_e = 0x01,
-    Flashing_e
-}DlState;
-
-
-boolean sdHasError;
-boolean sdFileIsOpened;
-
-File openFile;
-
-File file;
-
 TFT_eSPI tft = TFT_eSPI();
 
-void open_sd_file() {
-  Serial.print("Open SD File");
+// perform the actual update from a given stream
+void performUpdate(Stream &updateSource, size_t updateSize) {
+  tft.print("start update...");
+  if (Update.begin(updateSize)) {
+    size_t written = Update.writeStream(updateSource);
+    if (written == updateSize) {
+       Serial.println("Written : " + String(written) + " successfully");
+    }
+    else {
+       Serial.println("Written only : " + String(written) + "/" + String(updateSize) + ". Retry?");
+    }
+    if (Update.end()) {
+      Serial.println("update done!");
+      tft.println("update done!");
+      if (Update.isFinished()) {
+        Serial.println("Update successfully completed. Rebooting.");
+      }
+      else {
+        Serial.println("Update not finished? Something went wrong!");
+      }
+    }
+    else {
+       Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+    }
+  
+  }
+  else
+  {
+    Serial.println("Not enough space to begin OTA");
+  }
+}
+
+#define UPDATE_FILE_NAME "/update.bin"
+
+// check update.bin if available
+boolean updateFileIsAvailable(fs::FS &fs) {
   boolean ret = false;
-  openFile = SD.open(TEXT_FILE_NAME, FILE_READ);
-  if(openFile){
-    Serial.println("...succeeded");
-    sdFileIsOpened = true;
-  } else{
-    Serial.println("...failed");
-    openFile.close();
-  }
-}
-
-void close_sd_file() {
-  if(sdFileIsOpened){
-    Serial.println("Close SD File");
-    openFile.close();
-    sdFileIsOpened = false;
-  }
-}
-
-void progressFunc(DlState state, int percent){
-  Serial.printf("state = %d - percent = %d\n", state, percent);
-}
-
-void error(char *message){
-  // printf("%s\n", message);
-  Serial.printf("%s\n", message);
-}
-
-int readData(uint8_t *buffer, int bytes){
-  return file.read(buffer, bytes);
-}
-
-void startFl(void){
-  //write bin file to sdcard
-  file = SD.open("fw.bin", FILE_READ);
-}
-
-void endFl(void){
-  file.close();
-}
-
-int sd_firmupdate_start(DlInfo &info) {
-    int downloaded = 0;
-    int written = 0;
-    int total = 1;
-    uint8_t buff[1024] = { 0 };
-    size_t size = sizeof(buff);
-    int ret = 0;
-
-    error("Downloading ... done -> Flashing ..."); 
-    if (Update.begin(total, U_FLASH)){
-//        Update.setMD5(info.strMD5);
-        Update.setMD5("5EAD17D0BF2DA78FF0FBB41B0D5F53C7"); // sample
-        downloaded = 0;
-        int r;
-        startFl();
-        while (!Update.isFinished()) {
-            //read sdcard
-            r = readData(buff, size);
-            written = Update.write(buff, r);
-            if (written > 0) {
-                if(written != r){
-                    error("Flashing chunk not full ... warning!");
-                }
-                downloaded += written;
-                progressFunc(Flashing_e, (100*downloaded)/total);
-            } else {
-                error("Flashing ... failed!");
-                ret = -1;
-                break;
-            }
-        }
-        endFl();
-        if(downloaded == total && Update.end()){
-            error("Flashing ... done!");
-            delay(100);
-            ESP.restart();                
-        } else {
-            error("Flashing or md5 ... failed!"); 
-            ret = -1;
-        }
-    } else {
-        error("Flashing init ... failed!");
-        ret = -1;
-      } 
-
-    return ret;
-}
-
-
-int readln(File* df, char* buf, int len)
-{
-    *buf = '\0';
-    int pos = 0;    
-     
-    while (df->available()) {
-        char ch = df->read();
-        *(buf + pos) = ch;
-        if (ch == 0x0a) {
-            *(buf + pos + 1) = '\0';
-            return pos;
-        }
-        pos++;
-        if (pos >= len) {
-            Serial.println("Overflowed!!");
-            *buf = '\0';
-            return 0;
-        }
+  
+  File updateBin = fs.open(UPDATE_FILE_NAME);
+  if (updateBin) {
+    if(updateBin.isDirectory()){
+       Serial.println("Error, update.bin is not a file");
     }
-}
-
-#define CONFIG_BUF_SIZE 31
-File configFile;
-char configBuff[CONFIG_BUF_SIZE];
-
-void get_sd_config() {
-  Serial.print("Open SD CONFIG");
-  configFile = SD.open(CONFIG_FILE_NAME, FILE_READ);
-  if(configFile){
-    Serial.println("config:");
-    memset(configBuff, 0, CONFIG_BUF_SIZE);
-    if (readln(&configFile, configBuff, (CONFIG_BUF_SIZE-1)) > 0) {
-        Serial.print(configBuff);
+    else {
+      size_t updateSize = updateBin.size();
+      if (updateSize > 0) {
+         Serial.println("update.bin is available");
+         ret = true;
+      }
+      else {
+         Serial.println("Error, file is empty");
+      }
     }
-    configFile.close();
-  } else{
-    configFile.close();
-    Serial.println("...failed->create new file");
-    create_sd_config();
+    updateBin.close();
   }
+  else {
+    Serial.println("Could not load update.bin from sd root");
+  }
+
+  return ret;
 }
 
-void create_sd_config() {
-  Serial.println("Create SD CONFIG");
-  configFile = SD.open(CONFIG_FILE_NAME, FILE_WRITE);
-  configFile.print(SW_VERSION);
-  configFile.close();
+// check given FS for valid update.bin and perform update if available
+void updateFromFS(fs::FS &fs) {
+  File updateBin = fs.open(UPDATE_FILE_NAME);
+
+  Serial.println("Try to start update");
+  size_t updateSize = updateBin.size();
+  performUpdate(updateBin, updateSize);  
+  updateBin.close();
+  // whe finished remove the binary from sd card to indicate end of the process
+  fs.remove("/update.bin");
+
+  rebootEspWithReason(String("Reboot after Update")); 
 }
-
-const esp_partition_t *running;
-
-typedef enum {
-  Runnning_e = 0x01,
-  Fota_e  
-}SysState;
-SysState state = Runnning_e;
-char strMD5[50];
 
 void setup() {
-
- WiFi.disconnect(true); //disable wifi
-
- Serial.begin(115200);
-
- Serial.println("start");
-
- tft.init();
- tft.setRotation(3);
- tft.fillScreen(TFT_BLACK);
-
- sdHasError = false;
- sdFileIsOpened = false;
- spiSD.begin(SD_CLK, SD_MISO, SD_MOSI, SD_SS);
- if(!SD.begin( SD_SS, spiSD, SDSPEED)){
-   Serial.println("Card Mount Failed");
-   sdHasError = true;
- }
- else {
-   Serial.println("Card Mount Succeeded");
- }
-
- memset(strMD5, 0, 50);
-
-}
-
-char buf[100];
-
-void loop() {
-
-  get_sd_config();
-
-
-  Serial.println("display file name");
+  uint8_t cardType;
+  Serial.begin(115200);
+  Serial.println("Welcome to the SD-Update example!");
+  
+  tft.init();
+  tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK); // Set the font color and the background color
   tft.setTextSize(2);
   tft.setCursor(0, 0); // Set cursor at top left of screen
 
-  tft.println(); 
-  tft.print("File:");
-  tft.println(TEXT_FILE_NAME);
-
-  // const esp_partition_t *running = esp_ota_get_running_partition();
-  running = esp_ota_get_running_partition();
-  Serial.printf("Running partition type %d subtype %d (offset 0x%08x)\r\n",
-             running->type, running->subtype, running->address);
-
-  switch(state)
-  {
-    case Runnning_e:
-      Serial.println("state: Runnning_e");
-      break;
-    case Fota_e:
-      Serial.println("state: Fota_e");
-//      DlInfo info;
-//      info.strMD5 = strMD5;
-//      sd_firmupdate_start(info);
-      break;
-    default:
-      Serial.println("state: default");
-      break;
-  }
-
-  memset(buf, 0, 100);
-  sprintf(buf, "Running partition type %d", running->type);
-  tft.print(buf);
-  tft.println(); 
-  memset(buf, 0, 100);
-  sprintf(buf, "Running partition subtype %d", running->subtype);
-  tft.print(buf);
-  tft.println(); 
-  memset(buf, 0, 100);
-  sprintf(buf, "(offset 0x%08x)",
-             running->address);
-  tft.print(buf);
-  tft.println(); 
-
-  memset(buf, 0, 100);
-  sprintf(buf, "soft version: %s", SW_VERSION);
-  tft.print(buf);
-  tft.println(); 
-
-  memset(buf, 0, 100);
-  sprintf(buf, "sd soft version: %s", configBuff);
-  tft.print(buf);
-  tft.println(); 
-
-  delay(5000);
-
-  //retry establishing SD connection
-  if (sdHasError) {
-    //retry establishing SD connection
-    spiSD.begin(SD_CLK, SD_MISO, SD_MOSI, SD_SS);
-    if(!SD.begin( SD_SS, spiSD, SDSPEED)){
-     Serial.println("Card Mount Failed");
-    }
-    Serial.println("Card Mount Succeeded");
-    sdHasError = false;
-  }
-
-  Serial.println("display file contents");
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK); // Set the font color and the background color
-  tft.setCursor(0, 0); // Set cursor at top left of screen
-
-  if (sdHasError) {
-    tft.setTextSize(3);
-    tft.println("");
-    tft.println("cannot access SD File"); // println moves cursor down for a new line
-    Serial.println("cannot access SD File");
+  tft.print("sd card mount...");
+  spiSD.begin(SD_CLK, SD_MISO, SD_MOSI, SD_SS);
+  if(!SD.begin( SD_SS, spiSD, SDSPEED)){
+    tft.println("FAILED");
+    rebootEspWithReason("Card Mount Failed");
   }
   else {
-    open_sd_file();
-    
-    if (sdFileIsOpened) {
-      String str;
-      int readableSize = openFile.available();
-      tft.setTextSize(2);
-      while (readableSize) {
-        char readData = char(openFile.read());
-        if (readData > 31 && readData < 127) { // filter(displayable data)
-          str += readData;
-        }
-        else if (readData == '\r') {
-          tft.println(str);
-          Serial.println(str);
-          str = "";
-        }
-        readableSize = openFile.available();
+    tft.println("OK");
+    Serial.println("Card Mount Succeeded");
+    tft.print("check new fw");
+    tft.print("...");
+    if (updateFileIsAvailable(SD)) {
+      tft.println("OK");
+      int count;
+      for(count = 10; count >= 0; count--) {
+        Serial.printf("%d ", count);
+        delay(1000);
       }
-  
-      if (str.length() > 0) {
-        tft.println(str);
-        Serial.println(str);
-      }
-
-      close_sd_file();
+      Serial.println("Start SD-Update");
+      updateFromFS(SD);
     }
     else {
-      tft.setTextSize(3);
-      tft.println("");
-      tft.println("cannot open File"); // println moves cursor down for a new line
-      Serial.println("cannot open File");
+      tft.println("NG");
     }
   }
 
-  delay(5000);
+}
 
+void rebootEspWithReason(String reason){
+    Serial.println(reason);
+    delay(1000);
+    ESP.restart();
+}
+
+//will not be reached
+void loop() {
+  
 }
 
